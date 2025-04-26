891e75a81d11c5d94845c7c22657d98d22a08641
abdulaziz27 <doelziez212@gmail.com>
2025-04-26 10:02:18 +0700

    session 1: learn write and read file

--- session_1/input_output.py
+++ session_1/input_output.py
@@ -27,3 +27,19 @@ print("%s berusia %d tahun" % (nama, umur))  # format string lama
 print("Apel", "Mangga", "Jeruk", sep=" - ")  # Menggunakan separator
 print("Baris 1", end=" >>> ")  # Mengubah ending (default adalah new line)
 print("Masih di baris yang sama")
+
+
+# Menulis ke file
+with open("contoh.txt", "w") as file:
+    file.write("Ini adalah baris pertama.\n")
+    file.write("Ini adalah baris kedua.")
+
+# Membaca dari file
+with open("contoh.txt", "r") as file:
+    isi = file.read()
+    print(isi)
+    
+# "a"	Append: Menambahkan teks di akhir file (tanpa menghapus isi lama).
+# "r+"	Read + Write: Baca dan tulis (file harus sudah ada).
+# "w+"	Write + Read: Hapus isi lama, lalu baca/tulis.
+# "a+"	Append + Read: Tambah teks di akhir + baca.
